9179dcf54a4844a1b4a007f3ae86327474aab2d6
ReKri76 <anch85072@gmail.com>
2025-12-01 19:12:01 +0300

    main

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.25
 require (
 	github.com/gofiber/fiber/v2 v2.52.10
 	github.com/golang-jwt/jwt/v5 v5.3.0
+	github.com/lib/pq v1.10.9
+	golang.org/x/crypto v0.14.0
 )
 
 require (
--- main.go
+++ main.go
@@ -1,29 +1,45 @@
 package main
 
 import (
-	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"os"
 	"strings"
 	"time"
 
+	"golang.org/x/crypto/bcrypt"
+
+	"database/sql"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v5"
+	_ "github.com/lib/pq"
 )
 
 type DBrecord struct {
-	Id           int
 	Mail         string `json:"mail"`
 	Password     string `json:"password"`
 	Role         string `json:"role"`
 	RefreshToken string `json:"refresh_token"`
-	RefreshTime  string `json:"refresh_time"`
+	RefreshTime  int64  `json:"refresh_time"`
 }
 
-var key = make([]byte, 32)
-
-func init() {
-	if _, err := rand.Read(key); err != nil {
-		panic("Failed to generate JWT key: " + err.Error())
+func loadPrivateKey(filename string) (*rsa.PrivateKey, error) {
+	keyData, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
 	}
+	block, _ := pem.Decode(keyData)
+	if block == nil {
+		return nil, fmt.Errorf("failed to parse PEM block")
+	}
+	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	return privateKey, nil
 }
 
 func (src DBrecord) CreateJWT(ttl int) (string, error) {
@@ -32,7 +48,11 @@ func (src DBrecord) CreateJWT(ttl int) (string, error) {
 		"role":    src.Role,
 		"exp":     time.Now().Add(time.Hour * time.Duration(ttl)).Unix(),
 	}
-	return jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString(key)
+	key, err := loadPrivateKey("./keys/private.pem")
+	if err != nil {
+		return "", err
+	}
+	return jwt.NewWithClaims(jwt.SigningMethodRS256, claims).SignedString(key)
 }
 
 func main() {
@@ -42,19 +62,41 @@ func main() {
 		StrictRouting: false,
 	})
 
+	db, err := sql.Open("postgres", "host=localhost port=5432 user=postgres password=BhD13 dbname=postgres sslmode=disable")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	defer db.Close()
+
 	app.Post("/api/register", func(c *fiber.Ctx) error {
 		var data DBrecord
 		if err := c.BodyParser(&data); err != nil {
 			return err
 		}
+		if data.Role != "Author" && data.Role != "Reader" {
+			return c.Status(400).SendString("invalid role")
+		}
 		if !strings.Contains(data.Mail, "@") {
 			return c.Status(400).SendString("Mail address is not valid")
 		}
+		var exists bool
+		query := "SELECT EXISTS(SELECT 1 FROM users WHERE Mail = $1);"
+		err := db.QueryRow(query, data.Mail).Scan(&exists)
+		if err != nil {
+			return err
+		}
+		if exists {
+			return c.Status(403).SendString("Mail already used")
+		}
 
-		//И если data.Mail не содержится в базе данных
-		//Добавить в базу данных запись(пока не знаю как рабоать с postgresql здесь, завтра разберусь)
+		bytes, err := bcrypt.GenerateFromPassword([]byte(data.Password+data.Mail), 14)
+		if err != nil {
+			return err
+		}
+		data.Password = string(bytes)
 
-		access, err := data.CreateJWT(7)
+		access, err := data.CreateJWT(2)
 		if err != nil {
 			return c.Status(500).JSON(fiber.Map{
 				"error": "Could not create access token",
@@ -66,6 +108,24 @@ func main() {
 				"error": "Could not create refresh token",
 			})
 		}
+		data.RefreshToken = refresh
+		data.RefreshTime = time.Now().Add(time.Hour * time.Duration(24*7)).Unix()
+
+		insertQuery := `
+    INSERT INTO users (Mail, Password, Role, RefreshToken, RefreshTime)
+    VALUES ($1, $2, $3, $4, $5)`
+
+		_, err = db.Exec(insertQuery,
+			data.Mail,
+			data.Password,
+			data.Role,
+			data.RefreshToken,
+			data.RefreshTime,
+		)
+		if err != nil {
+			return err
+		}
+
 		c.Cookie(&fiber.Cookie{
 			Name:     "refresh_token",
 			Value:    refresh,
@@ -80,6 +140,60 @@ func main() {
 			"access_token": access,
 		})
 	})
+
+	app.Post("/api/auth/login", func(c *fiber.Ctx) error {
+		var src DBrecord
+		if err := c.BodyParser(&src); err != nil {
+			return err
+		}
+		var data DBrecord
+		query := "SELECT Mail, Role, Password FROM users WHERE Mail = $1"
+		err = db.QueryRow(query, src.Mail).Scan(&data.Mail, &data.Role, &data.Password)
+		if err != nil {
+			return c.Status(403).SendString("Invalid mail or password")
+		}
+		err = bcrypt.CompareHashAndPassword([]byte(data.Password), []byte(src.Password+src.Mail))
+		if err != nil {
+			return c.Status(403).SendString("Invalid mail or password")
+		}
+		access, err := data.CreateJWT(2)
+		if err != nil {
+			return c.Status(500).JSON(fiber.Map{
+				"error": "Could not create access token",
+			})
+		}
+		refresh, err := data.CreateJWT(24 * 7)
+		if err != nil {
+			return c.Status(500).JSON(fiber.Map{
+				"error": "Could not create refresh token",
+			})
+		}
+		data.RefreshToken = refresh
+		data.RefreshTime = time.Now().Add(time.Hour * time.Duration(24*7)).Unix()
+		insertQuery := "UPDATE users SET RefreshToken = $1, RefreshTime = $2 WHERE Mail = $3"
+		_, err = db.Exec(insertQuery,
+			data.RefreshToken,
+			data.RefreshTime,
+			data.Mail,
+		)
+		if err != nil {
+			return err
+		}
+		c.Cookie(&fiber.Cookie{
+			Name:     "refresh_token",
+			Value:    refresh,
+			Expires:  time.Now().Add(24 * 7 * time.Hour),
+			HTTPOnly: true,
+			Secure:   true,
+			SameSite: "Lax",
+		})
+
+		return c.Status(200).JSON(fiber.Map{
+			"message":      "Auth successful",
+			"access_token": access,
+		})
+	})
+
 	app.Listen(":8080")
 
 }
