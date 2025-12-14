42e0cc912451a117e2a750fdfea24496e3167a38
ReKri76 <anch85072@gmail.com>
2025-12-14 13:46:40 +0300

    main

--- tests/CreateJWT_test.go
+++ tests/CreateJWT_test.go
@@ -1,25 +1,39 @@
 package tests
 
 import (
-	"crypto/rsa"
-	"errors"
-	"fmt"
 	"goblog/keys"
 	"io"
+	"log"
 	"net/http"
+	"os"
 	"strings"
+	"testing"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
-func JWT_test(pub *rsa.PublicKey, private *rsa.PrivateKey) (bool, error) {
+func TestJWT(t *testing.T) {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	private, err := keys.LoadPrivateKey(os.Getenv("PRIVATE_KEY"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	pub, err := keys.LoadPublicKey(os.Getenv("PUBLIC_KEY"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	mail := "test"
 	role := "test"
 	record := keys.Record{mail, role}
 
 	token, err := record.CreateJWT(1337, private)
 	if err != nil {
-		return false, err
+		t.Fatalf("%v", err)
 	}
 
 	test := fiber.New()
@@ -37,86 +51,85 @@ func JWT_test(pub *rsa.PublicKey, private *rsa.PrivateKey) (bool, error) {
 
 	reqValid, err := http.NewRequest("GET", "/test", nil)
 	if err != nil {
-		return false, err
+		t.Fatalf("%v", err)
 	}
 
 	reqValid.Header.Set("Authorization", "Bearer "+token)
 
 	res, err := test.Test(reqValid)
 	if err != nil {
-		return false, err
+		t.Fatalf("%v", err)
 	}
 
 	if res.StatusCode != 200 {
-		return false, errors.New("[ERROR] Status incorrect. Code:1. Status: " + res.Status)
+		t.Errorf("Status incorrect. Code:1. Status: %v", res.Status)
 	}
 
 	body, err := io.ReadAll(res.Body)
 	defer res.Body.Close()
 	if err != nil {
-		return false, err
+		t.Fatalf("%v", err)
 	}
 
 	parts := strings.Split(string(body), " ")
 	if len(parts) != 2 {
-		return false, fmt.Errorf("[ERROR] Incorrect number of parts. Expected: 2, got: %d", len(parts))
+		t.Errorf("Incorrect number of parts. Expected: 2, got: %d", len(parts))
 	}
 
 	if parts[0] != mail {
-		return false, errors.New("[ERROR] Incorrect mail address")
+		t.Errorf("Incorrect mail address")
 	}
 
 	if parts[1] != role {
-		return false, errors.New("[ERROR] Incorrect role")
+		t.Errorf("Incorrect role")
 	}
 
 	reqInValid1, err := http.NewRequest("GET", "/test", nil)
 	if err != nil {
-		return false, err
+		t.Fatalf("%v", err)
 	}
 
 	reqInValid1.Header.Set("InvalidHeader", "Bearer "+token)
 
 	res, err = test.Test(reqInValid1)
 	if err != nil {
-		return false, err
+		t.Fatalf("%v", err)
 	}
 
 	if res.StatusCode != 401 {
-		return false, errors.New("[ERROR] Status incorrect. Code:2. Status: " + res.Status)
+		t.Errorf("Status incorrect. Code:2. Status: %v", res.Status)
 	}
 
 	reqInValid2, err := http.NewRequest("GET", "/test", nil)
 	if err != nil {
-		return false, err
+		t.Fatalf("%v", err)
 	}
 
 	reqInValid2.Header.Set("Authorization", token)
 
 	res, err = test.Test(reqInValid2)
 	if err != nil {
-		return false, err
+		t.Fatalf("%v", err)
 	}
 
 	if res.StatusCode != 401 {
-		return false, errors.New("[ERROR] Status incorrect. Code:3. Status: " + res.Status)
+		t.Errorf("Status incorrect. Code:3. Status: %v", res.Status)
 	}
 
 	reqInValid3, err := http.NewRequest("GET", "/test", nil)
 	if err != nil {
-		return false, err
+		t.Fatalf("%v", err)
 	}
 
 	reqInValid3.Header.Set("InvalidHeader", "Bearer "+token+"invalid_part")
 
 	res, err = test.Test(reqInValid3)
 	if err != nil {
-		return false, err
+		t.Fatalf("%v", err)
 	}
 
 	if res.StatusCode != 401 {
-		return false, errors.New("[ERROR] Status incorrect. Code:4. Status: " + res.Status)
+		t.Errorf("Status incorrect. Code:4. Status: %v", res.Status)
 	}
 
-	return true, nil
 }
